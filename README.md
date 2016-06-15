0d71912a4b13db956b2e71112255cefd5b0d16dd
Charlie Baxter and Charlie Ewel <charlie@>
2016-06-15 11:17:17 -0700

    add multiple players

--- README.md
+++ README.md
@@ -25,3 +25,11 @@ Sample Output: Turn count increases by 1.
 - The game keeps two total score tallies; one is altered on odd turns (Player 1), the other altered on even turns (Player 2).
 Sample Input: Turn count is 2 and the player holds.
 Sample Output: Temporary score is added to Player 2's game score.
+
+- Allow a user-defined number of players to play.
+Sample Input: 4 players
+Sample Output: Player 1, Player 2, Player 3, Player 4
+
+- Prompt the user for player names.
+Sample Input: Charlie
+Sample Output: Player 1 is Charlie
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var tempScore = 0;
 var turnCount = 1;
-var players=[0,0];
+var players=[0,0,0,0,0,0,0,0];
 
 //Business Logic
 
@@ -22,33 +22,29 @@ function roll () {
   }
 }
 
-function hold () {
-  if (turnCount % 2 === 0) {
-    player2.gameScore += tempScore;
-    if(player2.gameScore>=100) {
-      alert("Player 2 is victorious");
-      newGame();
-    } else{
-      tempScore = 0;
-      turnCount +=1;
-    }
-  } else {
-    player1.gameScore += tempScore;
-    if(player1.gameScore>=100) {
-      alert("Player 1 is victorious");
-      newGame();
-    } else {
-      tempScore = 0;
-      turnCount +=1;
+function hold (num) {
+  var thisTurn=turnCount;
+  // debugger;
+  for(i=1; i<=num; i++) {
+    if (thisTurn % num === (i%num)) {
+      players[i-1].gameScore += tempScore;
+      if(players[i-1].gameScore>=100) {
+        alert("Player" + (i) + "is victorious");
+        newGame();
+      } else {
+        tempScore = 0;
+        turnCount +=1;
+      }
     }
   }
 }
 
 function newGame () {
-  var player1 = new player(player1, 0)
-  players[0]=(player1)
-  var player2 = new player(player2, 0)
-  players[1]=(player2)
+  players=[];
+  var playerNumber = parseInt(prompt("How many players?"))
+  for (i=0; i<playerNumber; i++) {
+    players.push(new player("player" + (i+1),0));
+  }
   tempScore = 0;
   turnCount = 1;
   updateDisplay();
@@ -56,8 +52,10 @@ function newGame () {
 
 function updateDisplay() {
   $("#tempScore").text("Your current score is " + tempScore);
-  $("#gameScore1").text("Player 1's score is " + players[0].gameScore);
-  $("#gameScore2").text("Player 2's score is " + players[1].gameScore);
+  $("#playerScores p").remove();
+  for(i=0;i<players.length;i++) {
+    $("#playerScores").append("<p> Player " + (i + 1) + "'s score is " + players[i].gameScore) + " </p>";
+  };
   $("#currentTurn").text(turnCount);
 }
 
@@ -65,17 +63,13 @@ function updateDisplay() {
 $(document).ready(function() {
   // event.preventDefault()
   $("button#roll").click(function() {
-    player1=players[0];
-    player2=players[1];
     rollResult=roll();
     $("#rollResult").text("Your roll was " + rollResult);
     $("#tempScore").text("Your current score is " + tempScore);
     $("#currentTurn").text(turnCount);
   })
   $("button#hold").click(function() {
-    player1=players[0];
-    player2=players[1];
-    hold();
+    hold(players.length);
     $("#rollResult").text("You held your score.");
     updateDisplay();
   })
