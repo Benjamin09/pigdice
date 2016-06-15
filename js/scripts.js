fe88bddad08bc1866cf42b070c1cbf319c11507c
Charlie Baxter and Charlie Ewel <charlie@>
2016-06-15 09:58:26 -0700

    fix reset such that when a player wins it congratulates them and starts a new game

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 var tempScore = 0;
-var gameScore1 = 0;
-var gameScore2 = 0;
 var turnCount = 1;
+var players=[0,0];
 
 //Business Logic
+
+function player(name, score) {
+  this.playerName = name;
+  this.gameScore = score;
+}
+
 function roll () {
   var rollResult = Math.ceil(6*Math.random());
   if (rollResult === 1) {
   tempScore = 0;
   turnCount += 1;
+  alert("You rolled a one. Our condolences. Please relinquish the mouse to your opponent.")
   return rollResult;
   } else {
   tempScore += rollResult;
@@ -18,47 +24,64 @@ function roll () {
 
 function hold () {
   if (turnCount % 2 === 0) {
-    gameScore2 += tempScore;
+    player2.gameScore += tempScore;
+    if(player2.gameScore>=100) {
+      alert("Player 2 is victorious");
+      newGame();
+    } else{
+      tempScore = 0;
+      turnCount +=1;
+    }
   } else {
-    gameScore1 += tempScore;
+    player1.gameScore += tempScore;
+    if(player1.gameScore>=100) {
+      alert("Player 1 is victorious");
+      newGame();
+    } else {
+      tempScore = 0;
+      turnCount +=1;
+    }
   }
-  tempScore = 0;
-  turnCount +=1;
 }
 
 function newGame () {
-  gameScore1 = 0;
-  gameScore2 = 0;
+  var player1 = new player(player1, 0)
+  players[0]=(player1)
+  var player2 = new player(player2, 0)
+  players[1]=(player2)
   tempScore = 0;
   turnCount = 1;
+  updateDisplay();
 }
 
-
+function updateDisplay() {
+  $("#tempScore").text("Your current score is " + tempScore);
+  $("#gameScore1").text("Player 1's score is " + players[0].gameScore);
+  $("#gameScore2").text("Player 2's score is " + players[1].gameScore);
+  $("#currentTurn").text(turnCount);
+}
 
 //UI Logic
 $(document).ready(function() {
   // event.preventDefault()
   $("button#roll").click(function() {
+    player1=players[0];
+    player2=players[1];
     rollResult=roll();
     $("#rollResult").text("Your roll was " + rollResult);
     $("#tempScore").text("Your current score is " + tempScore);
     $("#currentTurn").text(turnCount);
   })
   $("button#hold").click(function() {
+    player1=players[0];
+    player2=players[1];
     hold();
     $("#rollResult").text("You held your score.");
-    $("#tempScore").text("Your current score is " + tempScore);
-    $("#gameScore1").text("Player 1's score is " + gameScore1);
-    $("#gameScore2").text("Player 2's score is " + gameScore2);
-    $("#currentTurn").text(turnCount);
+    updateDisplay();
   })
   $("button#newGame").click(function() {
     newGame();
+    $("#pig-dice-game").show();
     $("#rollResult").text("Starting a new game. Click Roll to roll the dice!");
-    $("#tempScore").text("Your current score is " + tempScore);
-    $("#gameScore1").text("Player 1's score is " + gameScore1);
-    $("#gameScore2").text("Player 2's score is " + gameScore2);
-    $("#currentTurn").text(turnCount);
   })
-
 });
