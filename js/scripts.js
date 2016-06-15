2369327a5824baaaad0bc2d94d3c2cc272775e24
Charlie Baxter and Charlie Ewel <charlie+charlie@>
2016-06-15 08:31:05 -0700

    display the user's temporary score

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
+var tempScore = 0;
+
 //Business Logic
 function roll () {
   var rollResult = Math.ceil(6*Math.random());
+  tempScore += rollResult;
   return rollResult;
 }
 
@@ -11,5 +14,6 @@ $(document).ready(function() {
   $("button#roll").click(function() {
   rollResult=roll();
   $("#rollResult").text("Your roll was " + rollResult);
+  $("#tempScore").text("Your current score is " + tempScore);
   })
 });
